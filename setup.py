f1b52abf86581296f6c1db16be05ba98fb8d16a7
Derek Vance <DACRepair@gmail.com>
2020-01-11 22:06:23 -0500

    Packaging...

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='discordbot',
-      version='1.0.4',
+      version='1.1.0',
       description='Bot Framework for Discord',
       author='DACRepair',
       author_email='DACRepair@gmail.com',
