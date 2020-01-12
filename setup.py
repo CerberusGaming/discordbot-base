dda8837581ad9b06e3f85d481c3f44d6e36e5c96
Derek Vance <DACRepair@gmail.com>
2020-01-11 21:36:38 -0500

    Packaging...

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from setuptools import setup
 
-setup(name='discordbot-base',
+setup(name='discordbot',
       version='1.0.2',
       description='Bot Framework for Discord',
       author='DACRepair',
@@ -11,8 +11,8 @@ setup(name='discordbot-base',
           'discord.py',
           'sqlalchemy'
       ],
-      packages=['discordbot-base',
-                'discordbot-base.Common',
-                'discordbot-base.Modules',
-                'discordbot-base.Modules.Settings',
-                'discordbot-base.Modules.Storage'])
+      packages=['discordbot_base',
+                'discordbot_base.Common',
+                'discordbot_base.Modules',
+                'discordbot_base.Modules.Settings',
+                'discordbot_base.Modules.Storage'])
