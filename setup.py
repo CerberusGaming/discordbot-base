43b1cfe7a2066794df815b10bb55162ddbb7d8aa
Derek Vance <DACRepair@gmail.com>
2020-01-11 21:22:41 -0500

    Changing to a package for better multi-use control

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+setup(name='discordbot-base',
+      version='1.0',
+      description='Bot Framework for Discord',
+      author='DACRepair',
+      author_email='DACRepair@gmail.com',
+      url='https://github.com/CerberusGaming/discordbot-base',
+      install_requires=[
+          'discord.py',
+          'sqlalchemy'
+      ],
+      packages=['App'])
